a6ec6e6e4fc9e1eb71d6de520622311965f5c499
darshanjain533 <darshanjain533@gmail.com>
2023-04-10 11:26:13 +0400

    initial commit

---
+++ demo/demo1.js
@@ -0,0 +1,13 @@
+const http = require('http');
+ 
+// Create a server object:
+http.createServer(function (req, res) {
+ 
+    // Write a response to the client
+    res.write('This is a sample demo application....');
+ 
+    // End the response
+    res.end();
+ 
+    // The server object listens on port 8080
+}).listen(8080);
